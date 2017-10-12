a9227671bf17056acdb3b0d39b019fe1e41fcdaf
thanh dat <thanhdat21293@gmail.com>
2017-10-12 11:11:06 +0700

    add LUIS

--- index.js
+++ index.js
@@ -17,10 +17,29 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users
 app.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+var bot = new builder.UniversalBot(connector, function (session, args) {
+    session.send('Xin lỗi, Bot không hiểu \'%s\' là gì. Vui lòng gõ lại.', session.message.text);
 });
 
-var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9a32c774-509b-45bc-b6f4-bc4cf288ca78?subscription-key=66efec890639470b967529f59ae34ecb&verbose=true&timezoneOffset=420&spellCheck=false&q=';
+//var bot = new builder.UniversalBot(connector);
+
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9a32c774-509b-45bc-b6f4-bc4cf288ca78?subscription-key=66efec890639470b967529f59ae34ecb&verbose=true&timezoneOffset=420&spellCheck=false&q=hi';
 bot.recognizer(new builder.LuisRecognizer(model));
 
+
+bot.dialog("road", [
+        (session, args, next) => {
+            console.log(args)
+            if(args.intent && args.intent.entities && args.intent.entities.length > 0){
+                next({respone: args.intent.entities[0].type})
+            }
+            else
+                next({respone: 'Không tìm thấy địa điểm bạn muốn tìm.'})
+        },
+        (session, results) => {
+            console.log(123, results.respone)
+            session.send(results.respone)
+        },
+    ]).triggerAction({
+        matches: "road"
+});
